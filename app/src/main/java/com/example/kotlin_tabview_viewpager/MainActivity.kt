25bb1e0762e0ab7af2c54253755722ebf90fcfc1
anik-paul-dev <anikpaul.dev24@gmail.com>
2025-02-02 00:20:34 +0600

    first commit

---
+++ app/src/main/java/com/example/kotlin_tabview_viewpager/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.kotlin_tabview_viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager)
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Tab 1"
+                1 -> tab.text = "Tab 2"
+                2 -> tab.text = "Tab 3"
+            }
+        }.attach()
+
+    }
+}
